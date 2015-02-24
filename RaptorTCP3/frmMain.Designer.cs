6be59adfce9cba2c77496a0c2a267d16955e1fcf
ccslabs <admin@ccs-labs.com>
2015-02-24 19:36:28 +0000

    Initial Commit

---
+++ RaptorTCP3/frmMain.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace RaptorTCP3
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.cmsSystem = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.ConOut = new System.Windows.Forms.RichTextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.operationToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.startToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.restartToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblConnections = new System.Windows.Forms.Label();
+            this.cmsSystem.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // cmsSystem
+            // 
+            this.cmsSystem.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.operationToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.exitToolStripMenuItem});
+            this.cmsSystem.Name = "cmsSystem";
+            this.cmsSystem.Size = new System.Drawing.Size(128, 54);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(127, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(124, 6);
+            // 
+            // ConOut
+            // 
+            this.ConOut.BackColor = System.Drawing.Color.Black;
+            this.ConOut.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.ConOut.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.ConOut.ForeColor = System.Drawing.Color.MediumTurquoise;
+            this.ConOut.Location = new System.Drawing.Point(0, 242);
+            this.ConOut.Name = "ConOut";
+            this.ConOut.ReadOnly = true;
+            this.ConOut.ShowSelectionMargin = true;
+            this.ConOut.Size = new System.Drawing.Size(1122, 231);
+            this.ConOut.TabIndex = 1;
+            this.ConOut.Text = "";
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.BackColor = System.Drawing.Color.Black;
+            this.panel1.ContextMenuStrip = this.cmsSystem;
+            this.panel1.Controls.Add(this.lblConnections);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1122, 236);
+            this.panel1.TabIndex = 2;
+            // 
+            // operationToolStripMenuItem
+            // 
+            this.operationToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.startToolStripMenuItem,
+            this.restartToolStripMenuItem});
+            this.operationToolStripMenuItem.Name = "operationToolStripMenuItem";
+            this.operationToolStripMenuItem.Size = new System.Drawing.Size(127, 22);
+            this.operationToolStripMenuItem.Text = "Operation";
+            // 
+            // startToolStripMenuItem
+            // 
+            this.startToolStripMenuItem.Name = "startToolStripMenuItem";
+            this.startToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.startToolStripMenuItem.Text = "Pause";
+            // 
+            // restartToolStripMenuItem
+            // 
+            this.restartToolStripMenuItem.Name = "restartToolStripMenuItem";
+            this.restartToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.restartToolStripMenuItem.Text = "Restart";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Connections:";
+            // 
+            // lblConnections
+            // 
+            this.lblConnections.AutoSize = true;
+            this.lblConnections.Location = new System.Drawing.Point(87, 9);
+            this.lblConnections.Name = "lblConnections";
+            this.lblConnections.Size = new System.Drawing.Size(13, 13);
+            this.lblConnections.TabIndex = 1;
+            this.lblConnections.Text = "0";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(1122, 473);
+            this.ContextMenuStrip = this.cmsSystem;
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.ConOut);
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "frmMain";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.cmsSystem.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ContextMenuStrip cmsSystem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.RichTextBox ConOut;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ToolStripMenuItem operationToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem startToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem restartToolStripMenuItem;
+        private System.Windows.Forms.Label lblConnections;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ RaptorTCP3/frmMain.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using NetComm;
+
+namespace RaptorTCP3
+{
+    public partial class frmMain : Form
+    {
+        delegate void SetTextCallback(string text);
+        delegate void SetLabelTextCallback(Label ctrl, string text);
+        NetComm.Host tcpServer = new Host(9119);
+
+        public frmMain()
+        {
+            InitializeComponent();
+            panel1.Top = 0;
+            panel1.Left = 0;
+            panel1.Width = this.Width;
+            panel1.Height = this.Height / 3;
+
+            ConOut.Left = 0;
+            ConOut.Top = this.Height / 3;
+            ConOut.Width = this.Width;
+            ConOut.Height = 2 * this.Height / 3;
+        }
+
+        #region cmsSystem Operations
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (tcpServer.Listening)
+            {
+                Broadcast("Wait"); // Tell all the Clients to wait till I return
+                DisconnectAll();
+                tcpServer.CloseConnection();
+
+            }
+            Application.Exit();
+        }
+
+        private void DisconnectAll()
+        {
+            Parallel.ForEach(tcpServer.Users, ClientID =>
+            {
+                tcpServer.DisconnectUser(ClientID.ToString());
+            });
+
+            //foreach(string ClientID in tcpServer.Users)
+            //{
+            //    tcpServer.DisconnectUser(ClientID);
+            //}
+        }
+        #endregion
+
+        #region fmrMain Events
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            // Subscribe to NetComm Events
+            SubscribeToNetCommEvents();
+
+            // Start Listening
+            StartServerListening();
+
+        }
+
+        private void StartServerListening()
+        {
+            Log("Server is Listening");
+            tcpServer.StartConnection();
+            Broadcast("Resume");  // Tell any waiting Clients they can resume communications.
+        }
+
+        private void SubscribeToNetCommEvents()
+        {
+            tcpServer.ConnectionClosed += tcpServer_ConnectionClosed;
+            tcpServer.DataReceived += tcpServer_DataReceived;
+            tcpServer.DataTransferred += tcpServer_DataTransferred;
+            tcpServer.errEncounter += tcpServer_errEncounter;
+            tcpServer.lostConnection += tcpServer_lostConnection;
+            tcpServer.onConnection += tcpServer_onConnection;
+
+        }
+
+        private void Broadcast(string Message)
+        {
+            tcpServer.Brodcast(GetBytes(Message));
+        }
+
+        void tcpServer_onConnection(string id)
+        {
+            SetLabel(lblConnections, tcpServer.Users.Count().ToString("N0"));
+            Log(id + " Connected");
+        }
+
+       
+        private void SetLabel(Label lbl, string message)
+        {
+            if (lbl.InvokeRequired)
+            {
+                SetLabelTextCallback d = new SetLabelTextCallback(SetLabel);
+                this.Invoke(d, new object[] { lbl, message });
+            }
+            else
+            {
+                lbl.Text = message;
+            }
+        }
+
+
+
+        void tcpServer_lostConnection(string id)
+        {
+            SetLabel(lblConnections, tcpServer.Users.Count().ToString("N0"));
+            try
+            {
+                Log(id + " Disconnected");
+            }
+            catch (System.ObjectDisposedException de)
+            {
+                Log("Disposed... " + de.Message);
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+
+        void tcpServer_errEncounter(Exception ex)
+        {
+            Log("Error: " + ex.Message);
+        }
+
+        void tcpServer_DataTransferred(string Sender, string Recipient, byte[] Data)
+        {
+            Log("Message Transfer Requested by " + Sender + " to " + Recipient + " Message = " + GetString(Data));
+        }
+
+        private enum ServerCommands
+        {
+            Successful,
+            Failed,
+        }
+
+        void tcpServer_DataReceived(string ID, byte[] Data)
+        {
+            string[] command = GetString(Data).Split(' ');
+            Log(ID + " Sent this Data: " + GetString(Data));
+
+            switch (command[0].Trim().ToLowerInvariant())
+            {
+                case "login":
+                    if (LoginSuccessful(GetString(Data)))
+                        Reply(ID, command[0], ServerCommands.Successful.ToString());
+                    else
+                        Reply(ID, command[0], ServerCommands.Failed.ToString());
+                    break;
+                case "register":
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private bool LoginSuccessful(string commandParams)
+        {
+            string[] command = commandParams.Trim().ToLowerInvariant().Split(' ');
+            if (command[0] == "login")
+            {
+                return Login(command[1], command[2]);
+            }
+            return false;
+        }
+
+        private bool Login(string p1, string p2)
+        {
+            return false;
+        }
+
+        private void Reply(string ID, string callingCommand, string Result)
+        {
+            //string cr = callingCommand + " " + Result;
+            //byte[] commandResult = GetBytes(cr);
+            //tcpServer.SendData(ID, commandResult);
+
+        }
+
+        void tcpServer_ConnectionClosed()
+        {
+            lblConnections.Text = tcpServer.Users.Count().ToString("N0");
+            Log("Connection Closed");
+        }
+
+        #endregion
+
+        #region Utilities
+
+        private void Log(string message)
+        {
+            if (!ConOut.IsDisposed)
+            {
+                if (this.ConOut.InvokeRequired)
+                {
+                    SetTextCallback d = new SetTextCallback(Log);
+                    this.Invoke(d, new object[] { message });
+                }
+                else
+                {
+                    ConOut.AppendText(DateTime.Now + "\t" + message + Environment.NewLine);
+                }
+            }
+        }
+
+        private string GetString(byte[] data)
+        {
+            return Encoding.UTF8.GetString(data, 0, data.Length);
+        }
+
+        private byte[] GetBytes(string data)
+        {
+            return Encoding.UTF8.GetBytes(data);
+        }
+        #endregion
+    }
+}
